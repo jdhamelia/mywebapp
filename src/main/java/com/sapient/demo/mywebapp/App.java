6d1b55240b5eab529b102a3041c629a8f29ef5c3
JASMIN DHAMELIA <jdhamelia@sapient.com>
2016-05-17 13:03:34 -0400

    changes for feature A

--- src/main/java/com/sapient/demo/mywebapp/App.java
+++ src/main/java/com/sapient/demo/mywebapp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Sapient World!" );
+        System.out.println( "Hello World!" );
     }
 }
